93133ad8d0b6edd46fe98537a7616a29bacd5db9
shahrukh <shahrukh@shahrukhs-MacBook-Air.local>
2017-11-19 15:47:44 +0530

    Changed settings

--- scheduler/scheduler/settings.py
+++ scheduler/scheduler/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '+xu&wad&!)kv0!a)vmnz^v(k!!mu4x&=)o5m=1t6=-exxt(_7k'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
