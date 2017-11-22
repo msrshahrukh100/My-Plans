8208c6352fe0e4435421a0799f9d5b709ddeaece
Shahrukh <msr.concordfly@gmail.com>
2017-11-22 08:20:40 +0530

    Added customizations to the admin

--- scheduler/tasks/admin.py
+++ scheduler/tasks/admin.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-from .models import Task, Schedule
+from .models import Task, Schedule, Customizations
 from django.contrib import admin
 
 # Register your models here.
@@ -13,5 +13,9 @@ class ScheduleManager(admin.ModelAdmin):
 	list_display = ['user' ,'date', 'task', 'status']
 	list_filter = ['user' ,'date', 'task', 'status']
 
+class CustomizationManager(admin.ModelAdmin):
+	list_display = ['color', 'border_color']
+
 admin.site.register(Task, TaskAdmin)
 admin.site.register(Schedule, ScheduleManager)
+admin.site.register(Customizations, CustomizationManager)
