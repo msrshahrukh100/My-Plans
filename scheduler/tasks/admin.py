fdbe7b1a7ed3bcf6e997fa36413ef3f625f7592d
Shahrukh <msr.concordfly@gmail.com>
2017-12-20 21:25:48 +0530

    Changed list editable

--- scheduler/tasks/admin.py
+++ scheduler/tasks/admin.py
@@ -7,14 +7,12 @@ from django.contrib import admin
 
 class TaskAdmin(admin.ModelAdmin):
 	list_display = ['content', 'slug', 'time', 'color']
+	list_editable = ['color']
 
 class ScheduleManager(admin.ModelAdmin):
 	list_display = ['user' ,'date', 'task', 'status']
 	list_filter = ['user' ,'date', 'task', 'status']
 
-class CustomizationManager(admin.ModelAdmin):
-	list_display = ['color', 'border_color']
-	list_editable = ['border_color']
 
 admin.site.register(Task, TaskAdmin)
 admin.site.register(Schedule, ScheduleManager)
