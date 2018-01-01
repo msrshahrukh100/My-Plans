dd4aa497f07ffd622d74ae2c35dd973b09b89922
Shahrukh <msr.concordfly@gmail.com>
2018-01-01 16:28:57 +0530

    Added the step any in max_weight

--- scheduler/utility_apps/gym_tracker/forms.py
+++ scheduler/utility_apps/gym_tracker/forms.py
@@ -6,7 +6,7 @@ class ExerciseForm(forms.ModelForm):
 		super(ExerciseForm, self).__init__(*args, **kwargs)
 		# self.fields['reps'].widget.attrs['class'] = 'col l6 m10 s12 offset-l3 offset-m1'
 		# self.fields['sets'].widget.attrs['class'] = 'col l6 m10 s12 offset-l3 offset-m1'
-		# self.fields['max_weight'].widget.attrs['class'] = 'col l6 m10 s12 offset-l3 offset-m1'
+		self.fields['max_weight'].widget.attrs['step'] = 'any'
 
 	class Meta:
 		model = Exercise
