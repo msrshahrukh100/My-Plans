7741012ccf1dafd9d007c251400663aea607500e
Shahrukh <msr.concordfly@gmail.com>
2018-03-09 09:51:57 +0530

    Added the day of week to list editable

--- scheduler/utility_apps/gym_tracker/admin.py
+++ scheduler/utility_apps/gym_tracker/admin.py
@@ -6,21 +6,21 @@ from .models import Exercise, Program, ProgramExcerciseMap, UserProgramMap
 
 # Register your models here.
 
+
 class ExerciseAdmin(admin.ModelAdmin):
 	model = Exercise
 	list_display = ['exercise_name', 'muscle_group', 'sets', 'reps', 'max_weight', 'order_of_performing']
 	list_editable = ['order_of_performing']
 	list_filter = ['muscle_group']
 
+
 class ProgramExerciseMapAdmin(admin.ModelAdmin):
 	model = ProgramExcerciseMap
-	list_display = ['program', 'day_name', 'weekday']
-	list_editable = ['day_name' ,'weekday']
+	list_display = ['program', 'day_name', 'day_of_week', 'weekday']
+	list_editable = ['day_name', 'weekday', 'day_of_week']
+
 
 admin.site.register(Exercise, ExerciseAdmin)
 admin.site.register(Program)
 admin.site.register(ProgramExcerciseMap, ProgramExerciseMapAdmin)
 admin.site.register(UserProgramMap)
-
-
-
